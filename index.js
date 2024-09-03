c2bd78d2f06a5d50f2794003d2fca53aa967b39c
donia <donia@gmail.com>
2024-09-03 19:26:47 +0300

    first commit DB Updated II

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const jsonServer = require("json-server");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/Memory"); // Use in-memory adapter
+const db = low(new FileSync());
+
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // Static JSON file
+const router = jsonServer.router(db); // In-memory, volatile data storage
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3500;
 
 server.use(middlewares);
 server.use(router);
+
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
